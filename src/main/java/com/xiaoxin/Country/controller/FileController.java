906e1447724b12ea109924198f7838484cd04ceb
xiaoxin179 <97381599+xiaoxin179@users.noreply.github.com>
2023-08-31 20:56:22 +0800

    文件上传接口完成

---
+++ src/main/java/com/xiaoxin/Country/controller/FileController.java
@@ -0,0 +1,67 @@
+package com.xiaoxin.Country.controller;
+
+import cn.hutool.Hutool;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.xiaoxin.Country.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.MulticastChannel;
+
+/**
+ * @author:XIAOXIN
+ * @date:2023/08/31
+ * 文件处理接口
+ **/
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileController {
+    private static final String FILE_DIR = "/files/";
+    @Value("${file.upload.path:}")
+    private String uploadPath;
+    @Value("${server.port:9090}")
+    private String serverPort;
+    @Value("${file.download.ip:localhost}")
+    private String downloadIp;
+
+    /*
+     *文件上传接口
+     */
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws IOException {
+        if (StrUtil.isBlank(uploadPath)) {
+            uploadPath = System.getProperty("user.dir");
+        }
+        String mainName = FileUtil.mainName(file.getOriginalFilename());
+        String endName = FileUtil.extName(file.getOriginalFilename());
+        String fileUnique = IdUtil.fastSimpleUUID();
+        String fileFullName = fileUnique + StrUtil.DOT + endName;
+
+        // 构建完整的文件路径
+        String fullFilePath = uploadPath + FILE_DIR + fileFullName;
+
+        String originalFileName = file.getOriginalFilename();
+        long size = file.getSize();
+        String name = file.getName();
+        log.info("{},{},{}", originalFileName, size, name);
+
+        File updateFile = new File(fullFilePath);
+        File parentFile = updateFile.getParentFile();
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        file.transferTo(updateFile);
+
+        return Result.success("http://" + downloadIp + ":" + serverPort + "/file/" + fileFullName);
+    }
+}
